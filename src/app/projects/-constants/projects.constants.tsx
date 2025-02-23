72f87524e1a7b5c87a3d97061153684441097bf9
Sam Bautista <tadeolinco@gmail.com>
2025-02-23 12:08:24 +0800

    Fix escaped apostrophes

--- src/app/projects/-constants/projects.constants.tsx
+++ src/app/projects/-constants/projects.constants.tsx
@@ -34,7 +34,7 @@ export const PROJECTS: ProjectType[] = [
     content: (
       <div className="space-y-4 pb-4">
         <p>
-          I've always wanted to create a portfolio website, but lacked the
+          I&apos;ve always wanted to create a portfolio website, but lacked the
           inspiration for the design. I wanted a gimmick. I like film, and I
           just recently watched a{" "}
           <a
@@ -47,9 +47,9 @@ export const PROJECTS: ProjectType[] = [
           of the business card scene in American Psycho. So that was a start.
         </p>
         <p>
-          Initially I just had a white version of the center card, and that's
-          it. Played around with some CSS rotations, some gimmicks with
-          subtitles, but ended up scrapping a lot of the ideas.
+          Initially I just had a white version of the center card, and
+          that&apos;s it. Played around with some CSS rotations, some gimmicks
+          with subtitles, but ended up scrapping a lot of the ideas.
         </p>
         <p>
           Took a break and visited Letterboxd, and found out that I could
@@ -59,10 +59,10 @@ export const PROJECTS: ProjectType[] = [
         </p>
         <p>
           So I built a webscraper using Playwright to automatically get each
-          film's poster from their site. What better way of making your site
-          pretty than using something that's already pretty. Made a carousel à
-          la netflix. Then hook up a window resize listener to check the number
-          of carousels to render.
+          film&apos;s poster from their site. What better way of making your
+          site pretty than using something that&apos;s already pretty. Made a
+          carousel à la netflix. Then hook up a window resize listener to check
+          the number of carousels to render.
         </p>
         <p>
           Got some feedback that the posters were a bit too distractingly
@@ -96,8 +96,8 @@ export const PROJECTS: ProjectType[] = [
           cards behind it, then calculating contrast values to know if the text
           should be black or white. Then another contrast check among the
           secondary colors, but now against the chosen text color to use as a
-          accent for the border and my name's shadow. I especially like how it
-          turned out for Old Boy.
+          accent for the border and my name&apos;s shadow. I especially like how
+          it turned out for Old Boy.
         </p>
         <p>
           Heavy insipiration from{" "}
@@ -153,16 +153,17 @@ export const PROJECTS: ProjectType[] = [
         </p>
 
         <p>
-          So the team decided take the request in, especially since this didn't
-          need effort from backend side.
+          So the team decided take the request in, especially since this
+          didn&apos;t need effort from backend side.
         </p>
 
         <p>
           My approach was that I wanted ultimate flexibility for their exports,
-          so I added a bunch of toggles. Don't want to include the chart title?
-          Okay. No borders? Sure. No filters? No problem. Consistent widths? EZ.
-          I even added an optional delay to when the picture snaps just in case
-          they wanted to show a tooltip in the exported image, for example.
+          so I added a bunch of toggles. Don&apos;t want to include the chart
+          title? Okay. No borders? Sure. No filters? No problem. Consistent
+          widths? EZ. I even added an optional delay to when the picture snaps
+          just in case they wanted to show a tooltip in the exported image, for
+          example.
         </p>
       </div>
     ),
@@ -314,9 +315,9 @@ export const PROJECTS: ProjectType[] = [
 
         <p>
           Next, I tried YNAB, and other simple budgetting apps. I found out
-          about myself that I didn't really want to budget, which is usually a
-          core functionality of these apps. I just wanted to track where I spend
-          my money, and then get insights from that data.
+          about myself that I didn&apos;t really want to budget, which is
+          usually a core functionality of these apps. I just wanted to track
+          where I spend my money, and then get insights from that data.
         </p>
 
         <p>
@@ -329,16 +330,17 @@ export const PROJECTS: ProjectType[] = [
 
         <p>
           And then a PWA was made using NextJS, vercel for hosting, and supabase
-          as the database. Didn't bother buying a domain since I never planned
-          on letting anyone else use it. When I open it up, it goes directly to
-          the create transcation page, and fills up any information that it can
-          already assume from other settings, like currency, current tags, etc.
-          Entering inputs smartly passes the focus onto the next input, and on
-          and on, until it the next enter would submit the transaction
-          altogether. Afterwards, I put my phone away and forget about it.
+          as the database. Didn&apos;t bother buying a domain since I never
+          planned on letting anyone else use it. When I open it up, it goes
+          directly to the create transcation page, and fills up any information
+          that it can already assume from other settings, like currency, current
+          tags, etc. Entering inputs smartly passes the focus onto the next
+          input, and on and on, until it the next enter would submit the
+          transaction altogether. Afterwards, I put my phone away and forget
+          about it.
         </p>
 
-        <p>I've been using it since May 2022 now.</p>
+        <p>I&apos;ve been using it since May 2022 now.</p>
       </div>
     ),
   },
